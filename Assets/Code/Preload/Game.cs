c9606f034a9086e652cdacd670bf16724bd56b45
Max <maxime___tremblay@hotmail.com>
2019-10-11 18:25:20 -0400

    Project Initialization

---
+++ Assets/Code/Preload/Game.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+static class Game {
+
+    public static AudioManager m_audio;
+
+    static Game() {
+		GameObject game = SafeFind("_app");
+
+        m_audio = (AudioManager) SafeComponent(game, "AudioManager");
+    }
+
+	private static GameObject SafeFind(string p_name) {
+		GameObject obj = GameObject.Find(p_name);
+
+		if(obj == null) Error("GameObject " + p_name + "  not in preload.");
+
+		return obj;
+	}
+
+	private static Component SafeComponent(GameObject p_obj, string p_name) {
+		Component comp = p_obj.GetComponent(p_name);
+
+		if(comp == null) Error("Component " + p_name + " not in preload.");
+
+		return comp;
+	}
+
+	private static void Error(string p_error) {
+		Debug.Log(">>> Cannot proceed... " + p_error);
+		Debug.Log(">>> Make sure you launch from the preload scene first!");
+	}
+}
