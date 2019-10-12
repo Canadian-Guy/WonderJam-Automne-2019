4e5dc8892bb3eb42bf9c70efb82243e23f32ee3a
Canadian.Guy <jason_589@live.ca>
2019-10-12 08:15:36 -0400

    Modification du script Resetable pour conserver le bon scale quand on instancie un nouvel objet.

--- Assets/Resetable.cs
+++ Assets/Resetable.cs
@@ -13,6 +13,6 @@ public class Resetable : MonoBehaviour
     public void Reset()
     {
         Destroy(m_ObjectToReset);   //Destroy the old boi
-        m_ObjectToReset = Instantiate(m_Prefab, gameObject.transform.position, gameObject.transform.rotation);    //Spawn a new boi, whomst becomes the new old boi
+        m_ObjectToReset = Instantiate(m_Prefab, gameObject.transform.position, Quaternion.identity, gameObject.transform);    //Spawn a new boi, whomst becomes the new old boi
     }
 }
