1ffc6bfedb70a586f60c3a6a23a4c763e2468683
Maxime <maxime_morette@hotmail.com>
2019-10-12 12:36:08 -0400

    Added a particle system on the heater

--- Assets/Code/Heater.cs
+++ Assets/Code/Heater.cs
@@ -8,6 +8,7 @@ public class Heater : MonoBehaviour
     public SpriteRenderer m_playerSpriteRenderer;
     [HideInInspector]
     public PlayerDeath m_playerDeath;
+    public ParticleSystem m_smokeSystem;
 
     // Start is called before the first frame update
     void Start()
@@ -21,11 +22,17 @@ public class Heater : MonoBehaviour
     {
         if (m_playerDeath.m_isPraisingTheSun)
         {
+            if(!m_smokeSystem.isEmitting)
+                m_smokeSystem.Play();
             m_playerSpriteRenderer.color = new Vector4(1, (float)(1 - m_playerDeath.m_expositionRate), (float)(1 - m_playerDeath.m_expositionRate), m_playerSpriteRenderer.color.a);
+            m_smokeSystem.emissionRate = (float)(100 * m_playerDeath.m_expositionRate);
         }
         else
         {
+            if (m_smokeSystem.isEmitting)
+                m_smokeSystem.Stop();
             m_playerSpriteRenderer.color = new Vector4(1, (float)(1 - m_playerDeath.m_expositionRate), (float)(1 - m_playerDeath.m_expositionRate), m_playerSpriteRenderer.color.a);
+            m_smokeSystem.emissionRate = (float)(100 * m_playerDeath.m_expositionRate);
         }
     }
 }
