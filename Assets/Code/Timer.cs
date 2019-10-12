0084c440b7a287a613473e1d752fada468000a16
Maxime <maxime_morette@hotmail.com>
2019-10-12 09:17:42 -0400

    Timer Script

---
+++ Assets/Code/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+
+    float time = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SetTimer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SetTimer()
+    {
+        gameObject.GetComponent<TMP_Text>().text = time.ToString();
+        time++;
+        yield return new WaitForSeconds(1);
+        StartCoroutine(SetTimer());
+    }
+}
