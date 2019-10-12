a4de3f449433a79208876b3a07790f535295d85f
Maxime <maxime_morette@hotmail.com>
2019-10-12 06:42:43 -0400

    Merge the develop in my branch I created before sleep

---
+++ Assets/Code/Flasher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flasher : MonoBehaviour
+{
+    [HideInInspector]
+    public SpriteRenderer m_playerSpriteRenderer;
+    [HideInInspector]
+    public PlayerDeath m_playerDeath;
+    [Range(0,1)]public float m_flashSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_playerSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        m_playerDeath = gameObject.GetComponent<PlayerDeath>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
