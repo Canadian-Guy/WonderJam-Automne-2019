a3111dab2ad60fe037c34e7e6720168c8076a824
Maxime <maxime_morette@hotmail.com>
2019-10-12 08:40:55 -0400

    Player flash when in the sun(with acceleration), trying deceleration, deceleration doesn't work for nonow, so I just reset the speed and acceleration

--- Assets/Code/PlayerDeath.cs
+++ Assets/Code/PlayerDeath.cs
@@ -62,6 +62,7 @@ public class PlayerDeath : MonoBehaviour
 
         transform.position = m_spawnPosition.position;
         m_expositionRate = 0d;
+        gameObject.GetComponent<Flasher>().m_flashSpeed = gameObject.GetComponent<Flasher>().m_baseFlashSpeed;
         //Instantiate(m_player, m_spawnPosition.position, Quaternion.identity);
     }
 
