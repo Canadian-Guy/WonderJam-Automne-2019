20b72bdd2ae3199438424a09284ebf541a5980be
Etienne <etienneemond97@hotmail.com>
2019-10-12 15:05:35 -0400

    SSBU on switch

---
+++ Assets/SwitchController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+
+public class SwitchController : MonoBehaviour
+{
+    public Animator animator;
+    public UnityEvent onToggle;
+
+    private bool toggled = false;
+
+    public void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Toggle()
+    {
+        toggled = !toggled;
+        onToggle.Invoke();
+    }
+
+
+    public bool getToggled() { return toggled; }
+}
