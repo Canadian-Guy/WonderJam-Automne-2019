da8285c07a2f6e3272b0cdcf4e3cab380688ecd4
Canadian.Guy <jason_589@live.ca>
2019-10-12 07:57:43 -0400

    Ajout de deux scripts qui permettent de reset certains objets. Un objet doit avoir un script LevelReset, à qui on feed un event à raise et un Player qui peut l'utiliser. Les objets à reset doivent avoir un objet qui sert de spawn, avec le script Resetable dessus, à qui on donne un préfab à spawn et un objet à destroy. Voir la scene DevTestJason pour un exemple d'utilisation.

---
+++ Assets/LevelReset.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelReset : MonoBehaviour
+{
+    [Tooltip("Player that has the ability to reset de level")]
+    public Player m_ResetPoweredPlayer;
+
+    public GameEvent m_ResetEvent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_ResetPoweredPlayer.m_rewiredPlayer.GetButtonDown("Interact"))
+        {
+            Reset();
+        }
+    }
+
+    public void Reset()
+    {
+        m_ResetEvent.Raise();
+    }
+}
---
+++ Assets/Resetable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resetable : MonoBehaviour
+{
+    public GameObject m_Prefab;
+    public GameObject m_ObjectToReset;
+
+    public void Reset()
+    {
+        Destroy(m_ObjectToReset);   //Destroy the old boi
+        m_ObjectToReset = Instantiate(m_Prefab, gameObject.transform.position, gameObject.transform.rotation);    //Spawn a new boi, whomst becomes the new old boi
+    }
+}
